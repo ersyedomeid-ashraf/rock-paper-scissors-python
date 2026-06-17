e4e246c3a5f9f62499a4090e9b51b6827a325976
Syed Omeid Ashraf <er.syedomeid@gmail.com>
2026-06-17 12:29:07 +0530

    Initial commit

---
+++ file-1.py
@@ -0,0 +1,36 @@
+"""
+Work flow of Project -
+
+1- Input from user (Rock,Paper,Scissors)
+2- Computer choice (Computer will choose randomly not conditionally)
+3- Result priint
+
+
+Cases : A - ROCK
+
+Rock - Rock = Tie
+Rock - Paper = Paper win
+Rock - Scissors = Rock win
+
+Cases : B - Paper
+
+Paper - Paper = Tie
+Paper - Rock = Paper win
+Paper - Scissors = Scissors win
+
+Cases : C - Scissors
+
+Scissors - Scissors = Tie
+Scissors - Rock = Rock win
+Scissors - Paper = Scissors win
+
+"""
+
+import random
+
+item_list = ["Rock", "Paper", "Scissors"]
+
+user_choice = input("Enter your move = Rock, Paper, Scissors = ")
+comp_choice = random.choice(item_list)
+
+print(f"user_choice = {user_choice}, computer choice = {comp_choice}")
